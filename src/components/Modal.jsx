aec1bc2b5216008a4b088c1ff5532dfc2b55378f
Abhishek Pal <abhishekpal0122@gmail.com>
2023-09-23 00:06:10 +0530

    added comment hello

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -2,8 +2,9 @@ import classes from './Modal.module.css';
 
 function Modal(props){
     // console.log("inside modal prop", props);
+    console.log("hello ");
    return <>
-      
+     
       <div className={classes.backdrop} onClick={props.changeModalVisibility}></div>
       <dialog open className={classes.modal}>{props.children}</dialog>
    </>
